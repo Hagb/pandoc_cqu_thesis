9bd8f508416e1583f2e9b8ad83f517e0d11b3fa6
Hagb Green <hagb_green@qq.com>
2021-05-21 12:53:55 +0800

    Improve label stripping

--- pandoc_word_helper/utils.py
+++ pandoc_word_helper/utils.py
@@ -2,14 +2,22 @@ import regex
 import panflute as pf
 _attr_regex = r'(?:#([^ \{\}]+)|(\.[^ \{\}]+|-))'
 attrs_regex = r'\{\s*' + f'(?:{_attr_regex}\\s+)*{_attr_regex}' + r'\s*\}'
-anchor_re = regex.compile('^.* ' + attrs_regex + r'\s*$')
+anchor_re = regex.compile('^' + attrs_regex + r'\s*$')
 
 
 def stripLabel(elems):
-    match = anchor_re.match(''.join(pf.stringify(elem) for elem in elems))
+    label_strs = []
+    for i in elems[::-1]:
+        if isinstance(i, (pf.Space, pf.Str)):
+            label_strs.append(pf.stringify(i))
+            if label_strs[-1].startswith('{'):
+                break
+        else:
+            return
+    match = anchor_re.match(''.join(label_strs[::-1]))
     if match:
-        while '{' not in pf.stringify(elems.pop()):
-            pass
+        for i in range(len(label_strs)):
+            elems.pop()
         return {
             'identifier': match[1+2] or match[1],
             'classes': [i[1:] or 'nonumbered' for i in match.captures(2) + match.captures(2+2)],
